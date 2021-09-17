5c339de0c4a2dc4d3d464a2e16cb0e276db119ee
SubarnoNR <subarno.nathroy@gmail.com>
2021-09-17 21:30:43 +0530

    waste5

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5^-etd!=m0njl_&e-7gdsjd9fokid5f=$drttw&5ebkmz*$0m@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['outlab04-django.herokuapp.com','127.0.0.1']
 
