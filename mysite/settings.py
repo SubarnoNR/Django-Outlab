03e8d8fab0c805b94230099607fabc493f9973bb
SubarnoNR <subarno.nathroy@gmail.com>
2021-09-17 18:40:42 +0530

    waste1

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-5^-etd!=m0njl_&e-7gdsjd9fokid5f=$drttw&5ebkmz*$0m@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
